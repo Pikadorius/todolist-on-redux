25cbf62f457f33a306071bcdabf223ea150ba969
Pikadorius <karpukea@yandex.by>
2022-12-04 18:19:57 +0300

    refactor container component

--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -2,22 +2,60 @@ import App from './App';
 import {connect} from 'react-redux';
 import {Dispatch} from 'redux';
 import {AppRootState} from './redux/store';
+import {
+    addTaskAC,
+    addTasksToTodolistAC,
+    changeTaskStatusAC,
+    changeTaskTitleAC, deleteTasksFromTodolistAC, InititalStateType,
+    removeTaskAC
+} from './reducers/tasksReducer';
+import {v1} from 'uuid';
+import {
+    addTodolistAC,
+    changeTodolistFilterAC,
+    changeTodolistTitleAC,
+    FilterValuesType,
+    removeTodolistAC, TodolistType
+} from './reducers/todolistsReducer';
 
 type MapStateType = {
-    state: AppRootState
+    todolists: TodolistType[]
+    tasks: InititalStateType
 }
-const mapStateToProps = (state:AppRootState) => {
+const mapStateToProps = (state: AppRootState): MapStateType => {
     return {
-        state:state
+        todolists: state.todolists,
+        tasks: state.tasks
     }
 }
 
 type MapDispacthType = {
-    dispatch: Dispatch
+    removeTask: (id: string, todolistId: string) => void
+    addTask: (title: string, todolistId: string) => void
+    changeStatus: (id: string, isDone: boolean, todolistId: string) => void
+    changeTaskTitle: (id: string, newTitle: string, todolistId: string) => void
+    addTodolist: (title: string) => void
+    removeTodolist: (id: string) => void
+    changeTodolistTitle: (id: string, title: string) => void
+    changeFilter: (value: FilterValuesType, todolistId: string) => void
 }
-const mapDispacthToProps = (dispatch: Dispatch) => {
+const mapDispacthToProps = (dispatch: Dispatch): MapDispacthType => {
     return {
-        dispatch: dispatch
+        removeTask: (id: string, todolistId: string) => dispatch(removeTaskAC(id, todolistId)),
+        addTask: (title: string, todolistId: string) => dispatch(addTaskAC(title, todolistId)),
+        changeStatus: (id: string, isDone: boolean, todolistId: string) => dispatch(changeTaskStatusAC(id, isDone, todolistId)),
+        changeTaskTitle: (id: string, newTitle: string, todolistId: string) => dispatch(changeTaskTitleAC(id, newTitle, todolistId)),
+        addTodolist: (title: string) => {
+            let newTodolistId = v1();
+            dispatch(addTodolistAC(title, newTodolistId))
+            dispatch(addTasksToTodolistAC(newTodolistId))
+        },
+        removeTodolist: (id: string) => {
+            dispatch(removeTodolistAC(id))
+            dispatch(deleteTasksFromTodolistAC(id))
+        },
+        changeFilter: (value: FilterValuesType, todolistId: string) => dispatch(changeTodolistFilterAC(value, todolistId)),
+        changeTodolistTitle: (id: string, title: string) => dispatch(changeTodolistTitleAC(id, title))
     }
 }
 export type AppType = MapStateType & MapDispacthType
