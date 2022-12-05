b1b0b6e56a4b3d99703aeca7227d0bac115f51d2
Pikadorius <karpukea@yandex.by>
2022-12-05 10:41:05 +0300

    change App to AppRedux

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,12 @@ import './index.css';
 import {createRoot} from 'react-dom/client';
 import store from './redux/store';
 import {Provider} from 'react-redux';
-import App from './App';
+import AppRedux from './AppRedux';
 
 const container = document.getElementById('root') as HTMLElement
 const root = createRoot(container);
 root.render(
     <Provider store={store}>
-        <App/>
+        <AppRedux/>
     </Provider>
 );
