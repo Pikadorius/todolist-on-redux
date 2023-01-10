18c8da7bebffda5d532e227f8df1bce1bc969a3b
Pikadorius <karpukea@yandex.by>
2023-01-10 19:20:08 +0300

    added middleware for correct thunk work

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,19 +1,28 @@
-import {combineReducers, legacy_createStore} from 'redux';
-import {tasksReducer} from '../reducers/tasksReducer';
-import {todolistsReducer} from '../reducers/todolistsReducer';
-import {authReducer} from '../reducers/authReducer';
+import {applyMiddleware, combineReducers, legacy_createStore} from 'redux';
+import {TasksActionsType, tasksReducer} from '../reducers/tasksReducer';
+import {TodoActionsType, todolistsReducer} from '../reducers/todolistsReducer';
+import thunkMiddleware, {ThunkDispatch} from 'redux-thunk';
 
 
 // combine reducers with Redux method
-const rootReducer=combineReducers({
+const rootReducer = combineReducers({
     tasks: tasksReducer,
     todolists: todolistsReducer
 })
+// type of state
 export type AppRootState = ReturnType<typeof rootReducer>
 
-
+const enchaced=applyMiddleware(thunkMiddleware)
 // create store with Redux method
-const store=legacy_createStore(rootReducer)
+const store = legacy_createStore(rootReducer, enchaced)
+
+// type of dispatch
+export type ActionsType = TodoActionsType & TasksActionsType
+
+
+//explanation from redux-tookit docs
+export type AppDispatchType = ThunkDispatch<AppRootState, unknown, ActionsType>
+
 // @ts-ignore
 window.store = store;
 
