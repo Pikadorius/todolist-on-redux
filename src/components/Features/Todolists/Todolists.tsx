a75699cf6862973c63872bb0bf0ef403907fe0af
Pikadorius <karpukea@yandex.by>
2023-01-25 13:29:45 +0300

    removed addItemForm into todolists

--- src/components/Features/Todolists/Todolists.tsx
+++ src/components/Features/Todolists/Todolists.tsx
@@ -1,15 +1,21 @@
 import {useSelector} from 'react-redux';
 import {AppRootState, useAppDispatch} from '../../../redux/store';
-import {fetchTodolists, TodolistDomainType} from './todolistsReducer';
-import React, {useEffect} from 'react';
+import {createTodolistTC, fetchTodolists, TodolistDomainType} from './todolistsReducer';
+import React, {useCallback, useEffect} from 'react';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import {TodolistRedux} from './Todolist/TodoListRedux';
+import {AddItemForm} from '../../common/AddItemForm/AddItemForm';
 
 
 const Todolists = ({demo=false}:{demo?:boolean}) => {
     const todolists = useSelector<AppRootState, TodolistDomainType[]>(state => state.todolists)
     const dispatch = useAppDispatch()
+
+
+    const addTodolist = useCallback((title: string) => {
+        dispatch(createTodolistTC(title))
+    }, [])
 
     useEffect(() => {
         if (!demo) {
@@ -19,6 +25,12 @@ const Todolists = ({demo=false}:{demo?:boolean}) => {
     }, [])
 
     return <Grid container spacing={3}>
+        <Grid container spacing={-2}>
+            <Paper style={{margin: '20px'}} elevation={3}>
+                <AddItemForm addItem={addTodolist}/>
+            </Paper>
+        </Grid>
+
         {
             todolists.map(tl => {
                 return <Grid item key={tl.id}>
